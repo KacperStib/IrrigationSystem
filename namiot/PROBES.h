fe0067254218891b7907db2283d10ac683cc0843
kapix. <120423027+KacperStib@users.noreply.github.com>
2024-08-12 12:27:57 +0200

    Create PROBES.h

---
+++ namiot/PROBES.h
@@ -0,0 +1,19 @@
+#define PROBE1
+#define PROBE2
+#define PROBE3
+
+float measurment(uint8_t PROBE){
+  float meas = analogRead(PROBE);
+  float val = 100 - (meas * 100 / 3.3);
+  return val;
+}
+
+float calculateAverage(){
+  float val1 = measruement(PROBE1);
+  float val2 = measruement(PROBE2);
+  float val3 = measruement(PROBE3);
+
+  float avg = (val1 + val2 + val3) / 3.0;
+
+  return avg;
+}
