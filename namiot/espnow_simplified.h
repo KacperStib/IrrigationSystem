7badc7d0f93c6c101932d6855ef4dc98ea09ce7f
kapix. <120423027+KacperStib@users.noreply.github.com>
2024-08-21 10:00:39 +0200

    Create espnow_simplified.h

---
+++ namiot/espnow_simplified.h
@@ -0,0 +1,52 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+typedef struct msgNamiot {
+  float lux;
+  float tempInside;
+  float rhInside;
+  float tempOutside;
+  float rhOutside;
+  uint8_t waterLvl;
+  uint8_t probe1;
+  uint8_t probe2;
+  uint8_t probe3;
+  bool onOff;
+  bool seqEnd;
+};
+
+typedef struct msgTrawnik {
+  bool onOff;
+  bool isRain;
+  bool seqEnd;
+};
+
+msgNamiot msg;
+
+uint8_t panel[] = {0x40, 0x4C, 0xCA, 0xF5, 0xA1, 0x94};
+
+void OnDataRecv(const uint8_t *mac_addr, const uint8_t *incomingData, int len) {
+  memcpy(&msg, incomingData, sizeof(msg));
+}
+
+void espnow_init(){
+  WiFi.mode(WIFI_STA);
+  if (esp_now_init() != ESP_OK) {
+    //Serial.println("ESP-NOW Init Failed");
+    return;
+  }
+  esp_now_register_recv_cb(OnDataRecv);
+}
+
+void addPeer(uint8_t device[]){
+  esp_now_peer_info_t peerInfo;
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  memcpy(peerInfo.peer_addr, device, 6);
+  esp_now_add_peer(&peerInfo);
+}
+
+void sendCommand(uint8_t* device, struct msgNamiot commandData) {
+  esp_now_send(device, (uint8_t *)&commandData, sizeof(commandData));
+}
