2a81722d95c27a91a03c9670d438ed71ce81fcc5
Kacper <ks300578@student.polsl.pl>
2024-08-22 17:46:10 +0200

    two-espnow

--- namiot/namiot.ino
+++ namiot/namiot.ino
@@ -2,29 +2,37 @@
 #include "TSL2591.h"
 #include "SHT41.h"
 #include "SHT31.h"
-
+#include "espnow_simplified.h"
 float lux, tIn, rhIn, tOut, rhOut;
 
 void setup() {
   Serial.begin(9600);
   I2Cinit();
   TSLconfig();
+  espnow_init();
+  addPeer(panel);
 }
 
 void loop() {
   lux = TSLreadLux();
   Serial.printf("Lux TSL2591: %f\n", lux);
+  msg.lux = lux;
 
   SHT41measurment(&tIn, &rhIn);
   Serial.printf("SHT41: T: %f RH: %f\n", tIn, rhIn);
+  msg.tempInside = tIn;
+  msg.rhInside = rhIn;
 
   SHT31heaterEnable();
   delay(500);
   SHT31heaterDisable();
   delay(5000);
   SHT31measurment(&tOut, &rhOut);
-
   Serial.printf("SHT31: T: %f RH: %f\n\n", tOut, rhOut);
+  msg.tempOutside = tOut;
+  msg.rhOutside = rhOut;
+
+  sendCommand(panel, msg);
 
   delay(2000);
 }
